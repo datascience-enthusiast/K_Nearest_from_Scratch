1f6579611ccb214caa8f211bc4631a51827090b9
Shadir <shadprojects@gmail.com>
2019-11-13 18:54:41 +0530

    Initial Commit

---
+++ rockstar_example.py
@@ -0,0 +1,51 @@
+import rockstar
+K_Nearest_from_Scratch = """
+class K_Nearest_Neighbors:
+
+    def __init__(self, k=3):
+        self.k = k
+
+    def fit(self, points):
+        self.points = points
+
+    def euclidean_distance(self, p, q):
+        return np.sqrt(np.sum(np.array(p) - np.array(q)) ** 2)
+
+    def predict(self, new_point):
+        distances = []
+
+        for category in self.points:
+            for point in self.points[category]:
+                distance = self.euclidean_distance(point, new_point)
+                distances.append([distance, category])
+
+        categories = [category[1] for category in sorted(distances)[:self.k]]
+        result = Counter(categories).most_common(1)[0][0]
+        return result
+       
+clf = K_Nearest_Neighbors(k=3)
+clf.fit(points)
+print(clf.predict(new_point))
+
+ax = plt.subplot()
+ax.grid(True, color='#323232')
+
+ax.set_facecolor('black')
+ax.figure.set_facecolor('#121212')
+ax.tick_params(axis='x', colors='white')
+ax.tick_params(axis='y', colors='white')
+
+[ax.scatter(point[0], point[1], color='#104DCA', s=60) for point in points['blue']]
+[ax.scatter(point[0], point[1], color='#EF6C35', s=60) for point in points['orange']]
+
+new_class = clf.predict(new_point)
+color = '#EF6C35' if new_class == 'orange' else '#104DCA'
+ax.scatter(new_point[0], new_point[1], color=color, marker='*', s=200, zorder=100)
+
+[ax.plot([new_point[0], point[0]], [new_point[1], point[1]], color='#104DCA', linestyle='--', linewidth=1) for point in points['blue']]
+[ax.plot([new_point[0], point[0]], [new_point[1], point[1]], color='#EF6C35', linestyle='--', linewidth=1) for point in points['orange']]
+
+plt.show()"""
+
+rock_it_bro = rockstar.RockStar(days=1900, file_name='K_Nearest_from_Scratch.py', code=K_Nearest_from_Scratch)
+rock_it_bro.make_me_a_rockstar()
